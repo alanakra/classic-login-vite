abb910db576f3dc73708aece7f2b9dade0669f89
Alan Akra <alanakra@hotmail.fr>
2025-10-16 23:14:03 +0200

    Add vite-plugin-mkcert for HTTPS support and update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
+import mkcert from 'vite-plugin-mkcert';
 export default defineConfig({
     server: {
+		https: true,
 		host: 'demo-register-client.local'
-	}
+	},
+	plugins: [mkcert()]
 })
