b1d7c84e8e18889a5549ea87466452bb64f7a1c3
Alan Akra <alanakra@hotmail.fr>
2025-10-18 21:02:14 +0200

    Add login form

---
+++ pages/js/login.js
@@ -0,0 +1,27 @@
+const loginForm = document.getElementById('login-form')
+loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const uname = document.getElementById('uname').value
+    const upassword = document.getElementById('upassword').value
+    const formData = new FormData()
+    formData.append('uname', uname)
+    formData.append('upassword', upassword)
+    
+    const response = await fetch('http://demo-register-client.local:8080/login.php', {
+        method: 'POST',
+        body: formData
+    })
+    const data = await response.text()
+    
+    const messageDiv = document.getElementById('message')
+    if (response.ok) {
+        messageDiv.textContent = data
+        messageDiv.style.color = ''
+    } else if (response.status === 400) {
+        messageDiv.textContent = data
+        messageDiv.style.color = 'red'
+    } else {
+        messageDiv.textContent = `Error: ${data}`
+        messageDiv.style.color = ''
+    }
+})
