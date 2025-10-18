3e34f63612e34856796939a21d6011818580e1f7
Alan Akra <alanakra@hotmail.fr>
2025-10-18 18:20:48 +0200

    Add Vite configuration for multi-page setup

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 export default defineConfig({
     server: {
 		host: 'demo-register-client.local'
+	},
+	root: resolve(__dirname, 'pages'),
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'pages/index.html'),
+				registration: resolve(__dirname, 'pages/registration.html')
+			}
+		}
 	}
 })
